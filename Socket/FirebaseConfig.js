6bd2924ed8a048a8664d98e8768e1967e04ea01a
Oluwatosin Oyekeye <oluwatosin@meta.com>
2024-07-27 23:48:51 -0700

    Testing path

--- Socket/FirebaseConfig.js
+++ Socket/FirebaseConfig.js
@@ -4,6 +4,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import {VITE_API_KEY, VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_MESSAGING_SENDER_ID,
+  VITE_APP_ID,
+  VITE_MEASUREMENT_ID} from "./env";
 
 
 const firebaseConfig = {
